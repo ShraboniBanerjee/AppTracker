53220fdf18d073a597ba1eba62f71ea59a398b28
ShraboniBanerjee <shilpabanerjee50@gmail.com>
2023-06-09 13:45:55 +0530

    new commit

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, StyleSheet, AppState } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 
 export default function App() {
+  const [appActivities, setAppActivities] = useState([]);
+
+  useEffect(() => {
+    // Add AppState change event listener
+    AppState.addEventListener('change', handleAppStateChange);
+
+    // Fetch initial app activity
+    fetchAppActivities();
+
+    // Poll every 5 seconds
+    const interval = setInterval(fetchAppActivities, 5000);
+
+    // Clean up interval and event listener on component unmount
+    return () => {
+      clearInterval(interval);
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
+  }, []);
+
+  const handleAppStateChange = (nextAppState) => {
+    // Fetch app activities on app state change
+    if (nextAppState === 'active') {
+      fetchAppActivities();
+    }
+  };
+
+  const fetchAppActivities = async () => {
+    try {
+      const currentApp = await DeviceInfo.getApplicationName();
+      const activity = {
+        app: currentApp,
+        activity: 'Some activity data',
+      };
+      setAppActivities((prevActivities) => [...prevActivities, activity]);
+    } catch (error) {
+      console.log('Error fetching app activities:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>App Activities</Text>
+      {appActivities.map((activity, index) => (
+        <View key={index} style={styles.activityContainer}>
+          <Text style={styles.appName}>{activity.app}</Text>
+          <Text style={styles.appActivity}>{activity.activity}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -13,8 +58,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  activityContainer: {
+    marginBottom: 16,
+  },
+  appName: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  appActivity: {
+    fontSize: 16,
   },
 });
